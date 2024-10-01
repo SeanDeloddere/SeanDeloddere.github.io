5e82b93f8e24db8e50578906ca169063f078f887
SeanDeloddere <s.deloddere@gmail.com>
2024-10-01 21:23:49 +0300

    adding collapse clicker

--- scripts/collapseSections.js
+++ scripts/collapseSections.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     var coll = document.getElementsByClassName("collapsible");
+    var toggles = document.getElementsByClassName("toggle-text");
+
     for (var i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
+        toggles[i].addEventListener("click", function() {
             var content = this.nextElementSibling;
             if (content.style.display === "block") {
                 content.style.display = "none";
+                this.textContent = "(expand)";
             } else {
                 content.style.display = "block";
+                this.textContent = "(collapse)";
             }
         });
     }
